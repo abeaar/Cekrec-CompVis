a816bc54515a8d66b0bde24ea8ecde5670ce13eb
AbrMac <abraarjh@gmail.com>
2026-04-29 11:20:44 +0700

    Initial Commit

---
+++ Cekrec/CekrecApp.swift
@@ -0,0 +1,17 @@
+//
+//  CekrecApp.swift
+//  Cekrec
+//
+//  Created by abr on 29/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CekrecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
