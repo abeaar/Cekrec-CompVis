a07db047292d0d1565c12a48f09a504c2523f4f9
tegar-ganang <tegarsatrio10@gmail.com>
2026-05-06 10:38:13 +0700

    Add comment Videooutputmanager

--- ViewModels/VideoOutputManager.swift
+++ ViewModels/VideoOutputManager.swift
@@ -1,27 +1,31 @@
-
 import AVFoundation
 
 extension CameraManager {
-
+    // Menambahkan video data output untuk memproses frame dari kamera
     func addVideoDataOutput() {
         let videoOutput = AVCaptureVideoDataOutput()
+        // membuang frame yang terlambat
         videoOutput.alwaysDiscardsLateVideoFrames = true
 
+        // format pixel
         videoOutput.videoSettings = [
             kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA
         ]
+        // queue untuk memproses frame
         let frameQueue = DispatchQueue(
             label: "com.cekrec.camera.videoOutput",
             qos: .userInitiated
         )
         videoOutput.setSampleBufferDelegate(self, queue: frameQueue)
 
+        // menambahkan video output ke session
         guard session.canAddOutput(videoOutput) else {
             print("[VideoOutputManager] Cannot add video data output.")
             return
         }
         session.addOutput(videoOutput)
 
+        // mengatur rotasi video output
         if let connection = videoOutput.connection(with: .video) {
             if #available(iOS 17.0, *) {
                 if connection.isVideoRotationAngleSupported(90) {
@@ -36,8 +40,9 @@ extension CameraManager {
     }
 }
 
+// Delegate untuk memproses frame dari video output
 extension CameraManager: AVCaptureVideoDataOutputSampleBufferDelegate {
-
+    // mengambil frame
     func captureOutput(
         _ output: AVCaptureOutput,
         didOutput sampleBuffer: CMSampleBuffer,
@@ -48,6 +53,7 @@ extension CameraManager: AVCaptureVideoDataOutputSampleBufferDelegate {
         }
     }
 
+    // ketika frame di-drop
     func captureOutput(
         _ output: AVCaptureOutput,
         didDrop sampleBuffer: CMSampleBuffer,
