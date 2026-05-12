3ab19c80d15fcea3719886db48d8387680d55ccc
AbrMac <abraarjh@gmail.com>
2026-05-12 16:12:48 +0700

    tambah file

---
+++ Views/GlassIconButton.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct GlassIconButton: View {
+    let icon: String
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            Image(systemName: icon)
+                .font(.body.weight(.semibold))
+                .foregroundStyle(.white)
+                .frame(width: 36, height: 36)
+                .glassEffect(in: .circle)
+        }
+    }
+}
