bff1a75f6f80226b68968845c769fdd8a15baa6e
AbrMac <abraarjh@gmail.com>
2026-06-01 15:47:30 +0700

    adding icon

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -68,7 +68,6 @@ struct ContentView: View {
             } else {
                 //UI callback semisal belum authorisasi Camera dari user
                 VStack {
-                    Text("apapun itu yabng bisa ditulis langsung")
                     Image(systemName: "camera.fill")
                         .font(.largeTitle)
                         .foregroundStyle(.gray)
@@ -99,11 +98,7 @@ struct ContentView: View {
                             .font(.caption)
                             .foregroundStyle(.white)
                             .frame(width: 40, height: 40)
-                            .glassEffect(in: .circle)
-                            .overlay(
-                                Circle()
-                                    .fill(.black.opacity(0.3))
-                            )
+                            .glassEffect(.regular.interactive(), in: .circle)
                     }
                     
                     Spacer()
@@ -114,7 +109,7 @@ struct ContentView: View {
                             .font(.caption)
                             .foregroundStyle(.white)
                             .frame(width: 40, height: 40)
-                            .glassEffect(in: .circle)
+                            .glassEffect(.regular.interactive(), in: .circle)
                     }
                 }
                 .padding(.horizontal, 16)
@@ -162,11 +157,7 @@ struct ContentView: View {
                                 .font(.title2)
                                 .foregroundStyle(.white)
                                 .frame(width: 45, height: 45)
-                                .glassEffect(in: .circle)
-                                .overlay(
-                                    Circle()
-                                        .fill(.black.opacity(0.3))
-                                )
+                                .glassEffect(.regular.interactive(), in: .circle)
                         }
                         .frame(width: 70)
                     }
